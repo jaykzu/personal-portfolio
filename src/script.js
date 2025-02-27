51d42d4706ae77a120e8d1b702c0ae4395e03f4e
jay <morterajk@gmail.com>
2025-02-27 03:45:54 -0800

    changed phrases, + new ones

--- src/script.js
+++ src/script.js
@@ -11,7 +11,7 @@ function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const phrases = ['program', 'hug my dog', 'study', 'code', 'tutor', 'eat', 'sleep', 'work out'];
+const phrases = ['study', 'hug my dog', 'program', 'eat', 'sleep', 'work out', 'code', 'practice MMA', 'listen to tunes', 'play sports', 'learn new things', 'play chess'];
 const el = document.getElementById('typewriter');
 
 let sleepTime = 100;
